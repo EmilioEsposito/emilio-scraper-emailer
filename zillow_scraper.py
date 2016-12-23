6b1c2188ccaae0cd985eb05f754685a7a8bfb83a
EmilioEsposito <espo412@gmail.com>
2016-12-23 00:33:54 -0500

    heroku ready

--- zillow_scraper.py
+++ zillow_scraper.py
@@ -35,42 +35,32 @@ subject = "Sernia Capital Zillow Update"
 # In[4]:
 
 import requests
-import time
+import datetime
 from bs4 import BeautifulSoup
  
 
 url = 'http://www.zillow.com/homedetails/320-S-Mathilda-St-APT-12-Pittsburgh-PA-15224/2097514490_zpid/?view=public'
 # url = 'http://www.zillow.com/homedetails/324-S-Mathilda-St-APT-12-Pittsburgh-PA-15224/2098920770_zpid/'
 # url = 'http://www.zillow.com/homedetails/332-S-Mathilda-St-APT-02-Pittsburgh-PA-15224/2097352140_zpid/?view=public'
-i=0
-while i<1000:
-    print i
 
-    response = requests.get(url)
+response = requests.get(url)
 
-    html_doc = response.content
-    soup = BeautifulSoup(html_doc, 'html.parser')
+html_doc = response.content
+soup = BeautifulSoup(html_doc, 'html.parser')
 
-    status_tag = soup.find("div", { "class" : "estimates" })
-    status_text = status_tag.text
+status_tag = soup.find("div", { "class" : "estimates" })
+status_text = status_tag.text
 
-    body = "null"
-    offmarket = False
-    if "For Rent" in status_text:
-        body = "The apartment is still For Rent... i="+str(i)+"\n"
-        if i%100==0:
-            mySendMail(sender, receivers, subject, body)
-    elif "Off Market" in status_text:
-        body = "\n**UPDATE\nThe apartment is now Off Market! i="+str(i)+"\n" + url
-        mySendMail(sender, receivers, subject, body)
-        break
-    print body
+body = "null"
+offmarket = False
+if "For Rent" in status_text:
+    body = "The apartment is still For Rent..."+str(datetime.datetime.now())+"\n"
+#    mySendMail(sender, receivers, subject, body)
+elif "Off Market" in status_text:
+    body = "\n**UPDATE\nThe apartment is now Off Market!"+str(datetime.datetime.now())+"\n" + url
+    mySendMail(sender, receivers, subject, body)
+print body
     
-    i+=1
-    
-    # wait 3 minutes
-    time.sleep(60*3)
-
 
 # In[ ]:
 
