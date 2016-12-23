a79a13a772594558d581c1ad2771a888ad03b9b2
EmilioEsposito <espo412@gmail.com>
2016-12-22 21:13:23 -0500

    commit

---
+++ zillow_scraper.py
@@ -0,0 +1,78 @@
+
+# coding: utf-8
+
+# # Setup Email
+# 
+
+# In[3]:
+
+import smtplib
+
+def mySendMail(sender, receivers, subject, body):
+    receivers_text = ", ".join(["<"+receiver+">" for receiver in receivers])
+
+    message = "From: <"+sender+">\nTo: "+receivers_text+"\nSubject: "+subject+"\n\n"+body+"\n"
+
+    try:
+        server = smtplib.SMTP('mail.smtp2go.com', 2525) #  8025, 587 and 25 can also be used. 
+        server.ehlo()
+        server.starttls()
+        server.ehlo()
+        server.login("espo412@gmail.com", "7aeESWAwHN3D")
+        server.sendmail(sender, receivers, message)
+        print "Successfully sent email"
+    except smtplib.SMTPException:
+        print "Error: unable to send email"
+        
+
+sender = 'espo412@gmail.com'
+receivers = ['emilio@serniacapital.com']
+subject = "Sernia Capital Zillow Update"
+
+
+# # Scrape Zillow continously
+
+# In[4]:
+
+import requests
+import time
+from bs4 import BeautifulSoup
+ 
+
+url = 'http://www.zillow.com/homedetails/320-S-Mathilda-St-APT-12-Pittsburgh-PA-15224/2097514490_zpid/?view=public'
+# url = 'http://www.zillow.com/homedetails/324-S-Mathilda-St-APT-12-Pittsburgh-PA-15224/2098920770_zpid/'
+# url = 'http://www.zillow.com/homedetails/332-S-Mathilda-St-APT-02-Pittsburgh-PA-15224/2097352140_zpid/?view=public'
+i=0
+while i<1000:
+    print i
+
+    response = requests.get(url)
+
+    html_doc = response.content
+    soup = BeautifulSoup(html_doc, 'html.parser')
+
+    status_tag = soup.find("div", { "class" : "estimates" })
+    status_text = status_tag.text
+
+    body = "null"
+    offmarket = False
+    if "For Rent" in status_text:
+        body = "The apartment is still For Rent... i="+str(i)+"\n"
+        if i%100==0:
+            mySendMail(sender, receivers, subject, body)
+    elif "Off Market" in status_text:
+        body = "\n**UPDATE\nThe apartment is now Off Market! i="+str(i)+"\n" + url
+        mySendMail(sender, receivers, subject, body)
+        break
+    print body
+    
+    i+=1
+    
+    # wait 3 minutes
+    time.sleep(60*3)
+
+
+# In[ ]:
+
+
+
